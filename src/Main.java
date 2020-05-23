1e98e491a9afa4ea112740fe3c21f019e44b693a
Müberra Çelik <muberracelik@icloud.com>
2020-05-23 23:47:30 +0300

    karşılama,giriş ve kayıt arayüzü tasarlandı. SQlite projeye bağlandı.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author bthn0
- */
+import java.awt.Toolkit;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+
     
+    public static Toolkit kit = Toolkit.getDefaultToolkit();
+    static AnaEkran ekran = new AnaEkran();
+    public static int ekranX;
+    public static int ekranY;
+    public static String path = "jdbc:sqlite:database/netflix.db";
+    public static Connection baglanti=null;
+    
+
+    public static void main(String[] args) {        
+        try {
+            baglanti=DriverManager.getConnection(path);
+            System.out.println("Database'a baglanildi");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        while (ekran.isActive()) {
+
+        }
+        System.exit(0);
+    }
+
 }
